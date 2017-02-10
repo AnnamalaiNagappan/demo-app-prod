82d641b1489eec2450c54d10243cf3d8bd0f8192
unknown <annamalai.nagappan@gramener.com>
2017-02-10 13:42:02 +0530

    ENH: Added routes

--- app.py
+++ app.py
@@ -1,14 +1,40 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
-
 @app.route('/')
-def hello():
-    return "Hello World!"
+def homepage():
+
+    title = "Epic Tutorials"
+    paragraph = ["wow I am learning so much great stuff!wow I am learning so much great stuff!wow I am learning so much great stuff!wow I am learning so much great stuff!","wow I am learning so much great stuff!wow I am learning so much great stuff!wow I am learning so much great stuff!wow I am learning so much great stuff!wow I am learning so much great stuff!wow I am learning so much great stuff!wow I am learning so much great stuff!wow I am learning so much great stuff!wow I am learning so much great stuff!"]
+
+    # try:
+    return render_template("index.html", title = title, paragraph=paragraph)
+    # except Exception, e:
+    #     return str(e)
 
 @app.route('/about')
-def about():
-    return "About us"
+def aboutpage():
 
-if __name__ == '__main__':
-    app.run()
+    title = "About this site"
+    paragraph = ["blah blah blah memememememmeme blah blah memememe"]
+
+    pageType = 'about'
+
+    return render_template("index.html", title=title, paragraph=paragraph, pageType=pageType)
+
+
+@app.route('/about/contact')
+def contactPage():
+
+    title = "About this site"
+    paragraph = ["blah blah blah memememememmeme blah blah memememe"]
+
+    pageType = 'about'
+
+    return render_template("index.html", title=title, paragraph=paragraph, pageType=pageType)
+
+
+
+if __name__ == "__main__":
+    app.run(debug = True, host='0.0.0.0', port=8080, passthrough_errors=True)
